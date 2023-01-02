fa2804bd8f93427fabbb0b38da44d9eeb18b608e
Valeria <valeria.dubina@isa.utm.md>
2023-01-02 17:28:29 +0200

    Add css for forgot password form

--- BookIT/bookit-frontend/src/components/auth/ForgotPasswordComponent.vue
+++ BookIT/bookit-frontend/src/components/auth/ForgotPasswordComponent.vue
@@ -1,17 +1,50 @@
 <template>
-  <div>
-    <span>Forgot Password </span>
-    <router-link :to="{name: 'Register'}">Register</router-link>
-    <router-link :to="{name: 'Login'}">Login</router-link>
+  <div class="auth-container">
+    <div class="auth-container__welcome-msg">Forgot password?</div>
+    <div class="auth-container__welcome-msg--tiny">No worries, just enter your email!</div>
+    <el-form class="auth-container__form">
+      <el-form-item>
+        <el-input type="text" v-model="email" placeholder="Email"/>
+      </el-form-item>
+    </el-form>
+    <el-button class="button--submit-auth-form"><i class="el-icon-right"/></el-button>
+    <div class="auth-container__redirect">
+        <router-link class="link" :to="{name: 'Login'}">Login</router-link>
+        <router-link class="link" :to="{name: 'Register'}">Create an account</router-link>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ForgotPasswordComponent"
+  name: "ForgotPasswordComponent",
+  data() {
+    return {
+      email: "",
+    };
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.auth-container__welcome-msg {
+  font-size: 1.7rem;
+
+  &--tiny {
+    font-size: 1rem;
+  }
+}
+
+.button--submit-auth-form {
+  position: absolute;
+  top: calc(15vh + 253px);
+  left: calc(50% - 40px);
+}
+
+@media only screen and (max-width: 768px) {
+  .button--submit-auth-form {
+    top: calc(30vh + 222px);
+  }
+}
 
 </style>
