c96e6400ebd2e8baf83128723f96ad562be6c6a3
Valeria <valeria.dubina@isa.utm.md>
2023-01-02 17:08:16 +0200

    Add css to register form

--- BookIT/bookit-frontend/src/components/auth/RegisterComponent.vue
+++ BookIT/bookit-frontend/src/components/auth/RegisterComponent.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
-    <span>Register</span>
-    <el-form :rules="rules" :model="user">
-      <el-form-item label="Email" prop="email">
-        <el-input type="text" v-model="user.email"/>
+  <div class="auth-container">
+    <div class="auth-container__welcome-msg">Register</div>
+    <div class="auth-container__welcome-msg--tiny">Some text needed here!</div>
+    <el-form class="auth-container__form--register" :rules="rules" :model="user">
+      <el-form-item prop="email">
+        <el-input type="text" v-model="user.email" placeholder="Email"/>
       </el-form-item>
-      <el-form-item label="Academic group" prop="group">
-        <el-select v-model="user.academicGroup">
+      <el-form-item prop="group">
+        <el-select v-model="user.academicGroup" placeholder="Select group">
           <el-option
               v-for="group in academicGroups"
               :key="group.id"
@@ -15,15 +16,18 @@
           />
         </el-select>
       </el-form-item>
-      <el-form-item label="Password" prop="password">
-        <el-input type="text" v-model="user.password"/>
+      <el-form-item prop="password">
+        <el-input type="text" v-model="user.password" placeholder="Password"/>
       </el-form-item>
-      <el-form-item label="Confirm password" prop="passwordConfirm">
-        <el-input type="text" v-model="user.confirmPassword"/>
+      <el-form-item prop="passwordConfirm">
+        <el-input type="text" v-model="user.confirmPassword" placeholder="Confirm password"/>
       </el-form-item>
     </el-form>
-    <router-link :to="{name: 'Login'}">Login</router-link>
-    <router-link :to="{name: 'ForgotPassword'}">Forgot password</router-link>
+    <el-button class="button--submit-auth-form"><i class="el-icon-right"/></el-button>
+    <div class="auth-container__redirect">
+      <router-link class="link" :to="{name: 'Login'}">LogIn</router-link>
+      <router-link class="link" :to="{name: 'ForgotPassword'}">Forgot password?</router-link>
+    </div>
   </div>
 </template>
 
@@ -32,9 +36,9 @@ export default {
   name: "RegisterComponent",
   data() {
     const confirmPasswordValidator = (rule, value, callback) => {
-      if(value === ""){
+      if (value === "") {
         callback(new Error("Please confirm password"));
-      } else if(value !== this.user.password) {
+      } else if (value !== this.user.password) {
         callback(new Error("Confirm password does not match"));
       } else {
         callback();
@@ -114,6 +118,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.button--submit-auth-form {
+  position: absolute;
+  top: calc(15vh + 435px);
+  left: calc(50% - 40px);
+}
 
+@media only screen and (max-width: 768px) {
+  .button--submit-auth-form {
+    top: calc(30vh + 370px);
+  }
+}
 </style>
