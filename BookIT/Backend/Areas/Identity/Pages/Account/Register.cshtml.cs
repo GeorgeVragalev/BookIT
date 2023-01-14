84c558f7ea4e57d0ac5ae09a6f03bf1adb09239f
Gherman Adrian <adrian.gherman@isa.utm.md>
2023-01-14 22:59:25 +0200

    Confirm email
    Confirm email

--- BookIT/Backend/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookIT/Backend/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
 using Backend.Entities.Users;
+using Backend.Models;
+using Backend.Services.EmailService;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -22,21 +24,21 @@ namespace Backend.Areas.Identity.Pages.Account
         private readonly IUserStore<User> _userStore;
         private readonly IUserEmailStore<User> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
-
+        private readonly IEmailService _emailService;
+        // private readonly IEmailSender _emailSender;
+        
         public RegisterModel(
             UserManager<User> userManager,
             IUserStore<User> userStore,
             SignInManager<User> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            ILogger<RegisterModel> logger, IEmailService emailService)
         {
             _userManager = userManager;
             _userStore = userStore;
             _emailStore = GetEmailStore();
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
+            _emailService = emailService;
         }
 
         /// <summary>
@@ -125,8 +127,7 @@ namespace Backend.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    _emailService.SendEmail(new Message(user.Email, "Confirm your email", $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>."));
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
