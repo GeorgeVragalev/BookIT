fdb2b120d9cd6274cd27794618deb0deda654817
George Vragalev <78079918+GeorgeVragalev@users.noreply.github.com>
2023-01-22 23:45:42 +0200

    event displayed from lessons

--- BookIT/Backend/Controllers/HomeController.cs
+++ BookIT/Backend/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
+using AutoMapper;
 using Backend.Entities.Shared;
+using Backend.Models;
+using Backend.Services.University.LessonService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers;
@@ -7,8 +10,19 @@ namespace Backend.Controllers;
 [AutoValidateAntiforgeryToken]
 public class HomeController : Controller
 {
+    private readonly ILessonService _lessonService;
+    private readonly IMapper _mapper;
+
+    public HomeController(ILessonService lessonService, IMapper mapper)
+    {
+        _lessonService = lessonService;
+        _mapper = mapper;
+    }
+
     public IActionResult Index()
     {
+        var lessons = _lessonService.GetAll();
+        ViewData["Events"] = _mapper.Map<IList<EventModel>>(lessons);
         return View();
     }
 
@@ -22,4 +36,20 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
     }
+    
+    [Route("findall")]
+    public IActionResult FindAllEvents()
+    {
+        var lessons = _lessonService.GetAll().Select(e => new
+        {
+            id = e.Id,
+            title = e.Name,
+            start = e.TimePeriod.StartTime.ToString("d") +" "+ e.TimePeriod.StartTime.ToString("t"),
+            end = e.TimePeriod.EndTime.ToString("d") +" "+ e.TimePeriod.EndTime.ToString("t"),
+            group = e.Group?.Name,
+            teacher = e.Teacher?.User?.GetFullName()
+        });
+        return new JsonResult(lessons);
+    }
+
 }
